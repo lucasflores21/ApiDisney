83e7850542cc0575ae2185438de7dc9885250b44
Lucas Flores <lucasflores.lg7@gmail.com>
2021-11-17 15:13:09 -0300

    Api Disney

---
+++ src/test/java/com/ApiDisney/ApiDisneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ApiDisney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDisneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
